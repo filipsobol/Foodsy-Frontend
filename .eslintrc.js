8512040aaabc235ce4751f6026a4c5a5096732d5
Filip Sobol <sobol.filip@gmail.com>
2020-01-11 16:17:25 +0100

    Fix indention rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  // add your custom rules here
-  rules: {
-  }
+    root: true,
+
+    env: {
+        browser: true,
+        node: true
+    },
+
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended'
+    ],
+    
+    // add your custom rules here
+    rules: {
+        "indent": ["error", 4]
+    }
 }
